1554d59dd816af9070466d76a2cd8bc49b9de148
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-12-04 23:11:57 +0200

    Streaming users in batches and processing them

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,26 @@
+from seed import connect_to_prodev
+
+def stream_users_in_batches(batch_size):
+    """Stream users in batches"""
+    connection = connect_to_prodev()
+    try:
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_data")
+        batch = []
+        for row in cursor:
+            batch.append(row)
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+
+        if batch:
+            yield batch
+    finally:
+        connection.close()
+
+def batch_processing(batch_size):
+    """Process users in batches"""
+    for batch in stream_users_in_batches(batch_size):
+        filtered_batch = [user for user in batch if int(user['age']) > 25]
+        for user in filtered_batch:
+            print(user)
