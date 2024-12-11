6e5abc37d548ac5725d3e9b399d2674b448f9fed
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-12-11 22:13:37 +0200

    context manager logic

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __enter__(self):
+        self.connection = sqlite3.connect('users.db')
+        return self.connection
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.connection.close()
+
+with DatabaseConnection() as connection:
+    cursor = connection.cursor()
+    query = cursor.execute('''SELECT * FROM users''')
+    print(query.fetchall())
